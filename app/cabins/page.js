813300fb161ab1932be4c00e25e120a92610fd6b
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-22 11:51:24 +0700

    feat: config image from supabase source, fetching cabins list

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,20 +1,19 @@
-import Counter from "@/app/_components/Counter";
-import CabinCard from "@/app/_components/CabinCard";
+import CabinCard from '@/app/_components/CabinCard';
+import { getCabins } from '../_lib/data-service';
 
 export const metadata = {
-  title: "Cabins",
+  title: 'Cabins',
 };
-
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
 
   return (
     <div>
-      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+      <h1 className='text-4xl mb-5 text-accent-400 font-medium'>
         Our Luxury Cabins
       </h1>
-      <p className="text-primary-200 text-lg mb-10">
+      <p className='text-primary-200 text-lg mb-10'>
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
@@ -24,7 +23,7 @@ export default function Page() {
       </p>
 
       {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
           {cabins.map((cabin) => (
             <CabinCard cabin={cabin} key={cabin.id} />
           ))}
