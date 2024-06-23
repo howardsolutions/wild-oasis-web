3d0fac2dbf9b3c1b8bebcce0ad40b7637cd979c0
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-23 18:09:31 +0700

    feat: getting the user session from authentication

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,12 +1,16 @@
 import DateSelector from './DateSelector';
 import ReservationForm from './ReservationForm';
 import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service';
+import { auth } from '../_lib/auth';
+import LoginMessage from './LoginMessage';
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await auth();
 
   return (
     <div className='grid grid-cols-2 border border-primary-800 min-h-[400px]'>
@@ -15,7 +19,11 @@ async function Reservation({ cabin }) {
         settings={settings}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm user={session.user} cabin={cabin} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
