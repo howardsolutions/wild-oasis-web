f00d8003ad4ab4c3e96b81f7f886da72321eb0aa
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-23 18:35:36 +0700

    feat: signOut server action

--- app/account/page.js
+++ app/account/page.js
@@ -6,7 +6,7 @@ export const metadata = {
 
 export default async function Page() {
   const session = await auth();
-  const firstName = user.session.name.split(' ').at(0);
+  const firstName = session.user.name.split(' ').at(0);
 
   return (
     <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
