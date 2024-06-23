36f1655ebdc6cc8fad8ce5df883d8639bdc2e702
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-23 15:06:08 +0700

    feat: Reservation form and Date selector - utilizing suspense feature for streaming

---
+++ app/_components/Reservation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import DateSelector from './DateSelector';
+import ReservationForm from './ReservationForm';
+import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service';
+
+async function Reservation({ cabin }) {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
+  return (
+    <div className='grid grid-cols-2 border border-primary-800 min-h-[400px]'>
+      <DateSelector
+        bookedDates={bookedDates}
+        settings={settings}
+        cabin={cabin}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
+
+export default Reservation;
