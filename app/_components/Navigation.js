6c20928b9f72164abb26ff4ff9ff5618557f4ddb
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-23 18:02:06 +0700

    feat: integrate next auth

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,32 +1,50 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { auth } from '../_lib/auth';
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   return (
-    <nav className="z-10 text-xl">
-      <ul className="flex gap-16 items-center">
+    <nav className='z-10 text-xl'>
+      <ul className='flex gap-16 items-center'>
         <li>
           <Link
-            href="/cabins"
-            className="hover:text-accent-400 transition-colors"
+            href='/cabins'
+            className='hover:text-accent-400 transition-colors'
           >
             Cabins
           </Link>
         </li>
         <li>
           <Link
-            href="/about"
-            className="hover:text-accent-400 transition-colors"
+            href='/about'
+            className='hover:text-accent-400 transition-colors'
           >
             About
           </Link>
         </li>
+
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href='/account'
+              className='hover:text-accent-400 transition-colors flex items-center gap-4'
+            >
+              <img
+                src={session.user.image}
+                alt={session.user.name}
+                className='h-8 rounded-full'
+                referrerPolicy='no-referrer'
+              />
+            </Link>
+          ) : (
+            <Link
+              href='/account'
+              className='hover:text-accent-400 transition-colors'
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
