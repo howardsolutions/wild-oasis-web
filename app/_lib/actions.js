784d36ccc20162722bb00820dd62dcd3c7f1dc81
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-24 16:59:00 +0700

    feat: create new reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -107,3 +107,28 @@ export async function updateReservation(formData) {
   // redirecting
   redirect('/account/reservations');
 }
+
+export async function createBooking(bookingData, formData) {
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in');
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get('numGuests')),
+    observations: formData.get('observations').slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: 'unconfirmed',
+  };
+
+  const { error } = await supabase.from('bookings').insert([newBooking]);
+
+  if (error) throw new Error('Booking could not be created');
+
+  revalidatePath(`/cabins/${bookingData.cabinId}`);
+
+  redirect('/cabins/thankyou');
+}
