52a61491be8116ee40d9113018f4113742290452
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-24 14:05:34 +0700

    feat: delete reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from 'next/cache';
 import { signIn, signOut, auth } from './auth';
 import { supabase } from './supabase';
+import { deleteBooking, getBookings } from './data-service';
 
 export async function signInAction() {
   await signIn('google', {
@@ -42,4 +43,27 @@ export async function updateGuest(formData) {
 
   // revalidate after update
   revalidatePath('/account/profile');
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+
+  if (!session) throw new Error('You must be logged in');
+
+  // Only allow the guest to delete his / her own reservation
+  const bookings = await getBookings(session.user.guestId);
+  const guestBookingIds = bookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error('You are NOT allowed to delete this booking!');
+  }
+
+  const { error } = await supabase
+    .from('bookings')
+    .delete()
+    .eq('id', bookingId);
+
+  if (error) throw new Error('Booking could not be deleted');
+
+  revalidatePath('/account/reservations');
 }
